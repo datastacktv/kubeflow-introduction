Fix softmax layer

01/simple_dnn_model.py
@@ -11,8 +11,8 @@ test_images = test_images / 255.0
 model = tf.keras.Sequential([
    tf.keras.layers.Flatten(input_shape=(28, 28)),
    tf.keras.layers.Dense(128, activation='relu'),
-   tf.keras.layers.Dense(10),
-   tf.keras.layers.Softmax()])
+   tf.keras.layers.Dense(10)
+   ])
 model.compile(optimizer='adam',
              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
              metrics=['accuracy'])
@@ -24,5 +24,7 @@ print('\nTest accuracy:', test_acc)
 #7.Save model
 model.save('my_model')
 #8.Run predictions
-predictions = model.predict(test_images)
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
 print('\nPrediction:',predictions[0])

03/hyperparameter/experiment_katib.py
@@ -10,9 +10,9 @@ def main(params):
     test_images = test_images / 255.0
     model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(cell_numbers, activation='relu'),
-    tf.keras.layers.Dense(10),
-    tf.keras.layers.Softmax()])
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)])
+    
     model.compile(optimizer='adam',
              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
              metrics=['accuracy'])

03/pipelines/define_compile_model/src/define_compile_model.py
@@ -7,8 +7,7 @@ def define_compile_model(model_path):
     model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10),
-    tf.keras.layers.Softmax()])
+    tf.keras.layers.Dense(10)])
     model.compile(optimizer='adam',
              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
              metrics=['accuracy'])